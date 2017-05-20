8baa3cbc45ca2bc2da056fd6a7e1942582c58f34
mgrzywa <mgrzywa@objectivity.co.uk>
2017-05-20 11:47:28 +0200

    Initial commit.

---
+++ build.ps1
@@ -0,0 +1,14 @@
+﻿$Global:ErrorActionPreference = 'Stop'
+$Global:VerbosePreference = 'Continue'
+
+Install-PackageProvider -Name NuGet -force
+Import-PackageProvider -Name NuGet -force
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+Install-Module PSDepend
+Invoke-PSDepend -Force
+
+Set-BuildEnvironment -Force
+
+Invoke-psake -buildFile "$PSScriptRoot\psake.ps1" -nologo -Verbose:$VerbosePreference
+exit ( [int]( -not $psake.build_success ) )
