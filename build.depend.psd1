4b3d3b877846f66796885d1c5e947b123a094964
mgrzywa <mgrzywa@objectivity.co.uk>
2017-05-20 17:27:28 +0200

    Set dependencies to fixed versions, improved performance of local builds.

--- build.depend.psd1
+++ build.depend.psd1
@@ -1,6 +1,6 @@
 @{
-  Psake = 'latest'
-  Pester = 'latest'
-  BuildHelpers = 'latest'
-  PSScriptAnalyzer = 'latest'
+  Psake = '4.6.0'
+  Pester = '4.0.3'
+  BuildHelpers = '0.0.45'
+  PSScriptAnalyzer = '1.13.0'
 }
--- build.ps1
+++ build.ps1
@@ -1,25 +1,54 @@
 ﻿$Global:ErrorActionPreference = 'Stop'
 $Global:VerbosePreference = 'SilentlyContinue'
 
-"Installing NuGet"
-Install-PackageProvider -Name NuGet -force | Out-Null
+### Prepare NuGet / PSGallery
+if (!(Get-PackageProvider | Where-Object { $_.Name -eq 'NuGet' })) {
+    "Installing NuGet"
+    Install-PackageProvider -Name NuGet -force | Out-Null
+}
+"Preparing PSGallery repository"
 Import-PackageProvider -Name NuGet -force | Out-Null
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') {
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+}
 
-"Installing PSDepend"
-Install-Module PSDepend
-"Installing build dependencies"
-Invoke-PSDepend -Force -Verbose
+### Install PSDepend
+$psDepend = Get-Module -Name PSDepend -ListAvailable
+if (!$psDepend) { 
+    "Installing PSDepend"
+    Install-Module PSDepend
+} 
+else {
+    "Using PSDepend $($psDepend.Version)"
+}
 
+### Install build dependencies if required
+$dependencies = Get-Dependency
+$needInvokePSDepend = $false
+foreach ($dep in $dependencies) {
+    $moduleVersions = Get-Module -Name $dep.DependencyName -ListAvailable | `
+        Select-Object -ExpandProperty Version | `
+        Foreach-Object { "$($_.Major).$($_.Minor).$($_.Build)" }
+    if (!($moduleVersions -contains $dep.Version)) {
+        $needInvokePSDepend = $true
+        break
+    }
+}
+if ($needInvokePSDepend) { 
+    "Installing build dependencies"
+    Invoke-PSDepend -Force -Verbose
+}
+
+### Install project dependencies
 $projectRequirementsFile = "$PSScriptRoot\..\requirements.psd1"
 if (Test-Path -Path $projectRequirementsFile) {
-  "Installing additional dependencies"
+  "Installing project dependencies"
   Invoke-PSDepend -Path $projectRequirementsFile -Force
 }
 
+### Run psake
 "Setting build environment"
 Set-BuildEnvironment -Path "$PSScriptRoot\.." -Force
-
 "Starting psake build"
-Invoke-psake -buildFile "$PSScriptRoot\psake.ps1" -nologo -Verbose:$VerbosePreference
+Invoke-psake -buildFile "$PSScriptRoot\psake.ps1" -nologo
 exit ( [int]( -not $psake.build_success ) )
